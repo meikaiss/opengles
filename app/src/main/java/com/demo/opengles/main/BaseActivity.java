284ae3866b417056f9d16671de67ced9a3ec3420
meikaiss <347287812@qq.com>
2021-11-22 21:12:46 +0800

    基类Activity增加返回键

--- app/src/main/java/com/demo/opengles/main/BaseActivity.java
+++ app/src/main/java/com/demo/opengles/main/BaseActivity.java
@@ -3,7 +3,9 @@ package com.demo.opengles.main;
 import android.app.Activity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,13 +20,26 @@ public class BaseActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
         String title = getIntent().getStringExtra("title");
         if (TextUtils.isEmpty(title)) {
             setTitle(getClass().getSimpleName());
         } else {
             setTitle(title);
         }
-
     }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
