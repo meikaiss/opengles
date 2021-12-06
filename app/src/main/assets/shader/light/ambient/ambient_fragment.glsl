871477ca801fd0afde58923074f2c306799f831c
meikai <347287812@qq.com>
2021-12-06 23:12:28 +0800

    冯氏光照模型原理的理解

--- app/src/main/assets/shader/light/ambient/ambient_fragment.glsl
+++ app/src/main/assets/shader/light/ambient/ambient_fragment.glsl
@@ -4,6 +4,7 @@ varying vec3 ambient;
 
 void main() {
     //*表示的是各个分量相乘，就能得到环境光与物体材质颜色混合的效果
+    //点乘用内置函数dot，叉乘用内置函数cross，分量相乘用*
     //冯氏光照模型的原理理解参考 https://zhuanlan.zhihu.com/p/427477685
     vec3 finalColor = ambient * vec3(vColor);
     gl_FragColor = min(vec4(finalColor, vColor.a), vec4(1.0));
